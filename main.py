545a4541c7398d5e34e3f7e785839535d89692e4
mcgue <mcgu@protonmail.com>
2022-08-30 13:11:26 -0700

    Initial

---
+++ main.py
@@ -0,0 +1,11 @@
+# Simulate Dice Roll
+
+def print_hi(player_name):
+    # Greet player
+    print(f'Hi, {player_name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    name_input = input('Ready to play? Enter your name: ')
+    print_hi(name_input)
