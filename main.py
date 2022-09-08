f479c35f3bc261fea381b7a732b8310acfa748c4
mcgue <mcgu@protonmail.com>
2022-09-08 11:02:46 -0700

    finish up comments and formatting

--- main.py
+++ main.py
@@ -2,20 +2,19 @@
 # import modules
 import random
 
-
 def print_hi(player_name):
     # Greet player
     print(f'Hi, {player_name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 def roll_dice(max):
     max = int(max)
     return random.randint(1, max)
 
-
-# Press the green button in the gutter to run the script.
+# Run
 if __name__ == '__main__':
+    # Get player's name
     name_input = input('Ready to play? Enter your name: ')
+
     print_hi(name_input)
     dice_max = input('How many sides should the dice have? ')
     die_1 = roll_dice(dice_max)
